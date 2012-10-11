4cc25a58ec59e1a6d04a6062a4d0c156e62d730f
Jon Faulkenberry <jon.faulkenberry@buildingi.com>
2012-10-11 11:14:05 -0700

    Project skeleton generated
    Used command "rails new glister"

---
+++ glister/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glister::Application.config.secret_token = '7f56ab457a056733c5fcfc0f14ffbd723a4df33d224489cd8958bb41ee09a7bb15cd79232f8e665062f3daf9a23ed01ee7f5aaebdd9d300559352185b367e248'
